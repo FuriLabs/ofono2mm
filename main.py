87b5b59f1626f970cc254dd503a2c6e893353db4
Jesús Higueras <jesus@furilabs.com>
2024-09-27 22:58:47 +0200

    main: stop iterating over a dict we're changing

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class MMInterface(ServiceInterface):
 
         for path, modem in self.modems.items():
             modem.unexport_mm_interface_objects()
-            self.modems.pop(path)
+        self.modems.clear()
 
         self.loop.create_task(self.bus.release_name('org.freedesktop.ModemManager1'))
 
