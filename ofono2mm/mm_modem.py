9a1880d6772ae8a1906ed7c9e2d584ad28522d6b
Jesús Higueras <jesus@furilabs.com>
2024-10-03 22:01:13 +0200

    mm_modem: don't allow SetCurrentModes

--- ofono2mm/mm_modem.py
+++ ofono2mm/mm_modem.py
@@ -1017,6 +1017,10 @@ class MMModemInterface(ServiceInterface):
         ofono2mm_print(f"Setting current modes to {modes}", self.verbose)
 
         if modes in self.props['SupportedModes'].value:
+            if True:
+                # Do nothing for now. This doesn't work well.
+                return
+
             if modes[1] == 16:
                 await self.ofono_proxy['org.ofono.RadioSettings'].call_set_property('TechnologyPreference', Variant('s', 'nr'))
             if modes[1] == 8:
