1fd0b1511460012d31d7e102211837e2e94f439b
Bardia Moshiri <bardia@furilabs.com>
2025-08-08 18:07:14 -0400

    mm_bearer: emit a signal on ports change
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- ofono2mm/mm_bearer.py
+++ ofono2mm/mm_bearer.py
@@ -258,6 +258,7 @@ class MMBearerInterface(ServiceInterface):
                 self.emit_properties_changed({'Interface': value.value['Interface'].value})
                 if [value.value['Interface'].value, 2] not in self.mm_modem.props['Ports'].value:
                     self.mm_modem.props['Ports'].value.append([value.value['Interface'].value, 2]) # port type AT MM_MODEM_PORT_TYPE_AT
+                    self.mm_modem.emit_properties_changed({'Ports': self.mm_modem.props['Ports'].value})
             if 'Method' in value.value:
                 if value.value['Method'].value == 'static':
                     self.props['Ip4Config'].value['method'] = Variant('u', 2) # static MM_BEARER_IP_METHOD_STATIC
