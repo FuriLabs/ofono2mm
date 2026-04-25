1ca1729230c2d3a379371d0c00f7b65ed110a53f
Bardia Moshiri <bardia@furilabs.com>
2026-04-24 23:55:02 -0400

    logging: fix frame leak
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- ofono2mm/logging.py
+++ ofono2mm/logging.py
@@ -6,21 +6,29 @@ def ofono2mm_print(message, verbose):
         return
 
     frame = currentframe()
-    caller_frame = frame.f_back
-    if 'self' in caller_frame.f_locals:
-        cls = caller_frame.f_locals['self']
-        cls_name = cls.__class__.__name__
+    try:
+        caller_frame = frame.f_back
+        if not caller_frame:
+            print(f"{time()} {message}", flush=True)
+            return
 
-        if hasattr(cls, 'modem_name'):
-            obj_path = getattr(cls, 'modem_name', 'Unknown')
-        elif hasattr(cls, 'voicecall'):
-            obj_path = getattr(cls, 'voicecall', 'Unknown')
+        if 'self' in caller_frame.f_locals:
+            cls = caller_frame.f_locals['self']
+            cls_name = cls.__class__.__name__
+
+            if hasattr(cls, 'modem_name'):
+                obj_path = getattr(cls, 'modem_name', 'Unknown')
+            elif hasattr(cls, 'voicecall'):
+                obj_path = getattr(cls, 'voicecall', 'Unknown')
+            else:
+                obj_path = 'Unknown'
+
+            func_name = caller_frame.f_code.co_name
+            full_message = f"{cls_name}({obj_path}).{func_name}: {message}"
         else:
-            obj_path = 'Unknown'
+            func_name = caller_frame.f_code.co_name
+            full_message = f"{func_name}: {message}"
 
-        func_name = caller_frame.f_code.co_name
-        full_message = f"{cls_name}({obj_path}).{func_name}: {message}"
-    else:
-        func_name = caller_frame.f_code.co_name
-        full_message = f"{func_name}: {message}"
-    print(f"{time()} {full_message}")
+        print(f"{time()} {full_message}", flush=True)
+    finally:
+        del frame
